3fdde85610e600442fb1cb8c6b30dacbc759d46c
Dean Diaz <dean.diaz001@gmail.com>
2021-11-19 00:49:59 -0500

    modal feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Banner from "./components/Banner/Banner";
+import BannerOne from "./components/BannerOne/BannerOne";
 import Footer from "./components/Footer/Footer";
-
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
-      <Banner />
+      <BannerOne />
       <Footer />
     </div>
   );
---
+++ src/components/BannerOne/BannerOne.js
@@ -0,0 +1,53 @@
+import "./BannerOne.scss";
+import React, { Component } from "react";
+import Modal from "../BannerModal/Modal";
+
+export default class BannerOne extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+  showModal = () => {
+    this.setState({ show: true });
+  };
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      <div className="banner-one">
+        <h1 className="banner-one__title">Let's get to know each other</h1>
+        <p className="banner-one__text">
+          Answer a couple of questions, and we'll give you a plan curated to
+          your personal journey into crypto.
+        </p>
+
+        <div className="banner-one__container">
+          <Modal show={this.state.show} handleClose={this.hideModal}>
+            <p>Modal</p>
+          </Modal>
+          <div
+            className="banner-one__card deleteModal__button"
+            onClick={this.showModal}
+          >
+            <h1 className="banner-one__card-title">Beginner Trader</h1>
+            <p className="banner-one__card-subtitle">I'm new here!</p>
+          </div>
+          <div className="banner-one__card" onClick={this.showModal}>
+            <h1 className="banner-one__card-title">Intermediate Trader</h1>
+            <p className="banner-one__card-subtitle">I know a little.</p>
+          </div>
+          <div className="banner-one__card" onClick={this.showModal}>
+            <h1 className="banner-one__card-title">Advanced Trader</h1>
+            <p className="banner-one__card-subtitle">Don't @ me</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
